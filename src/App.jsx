528120f99477d859556972e3f8df3553e9b99a45
Yash Gupta <145326279+yash70g@users.noreply.github.com>
2024-07-20 23:38:17 +0530

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+import './App.css'
+import Navbar from './components/Navbar'
+import Manager from './components/Manager'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <Navbar/>
+      <Manager/>
+    </>
+  )
+}
+
+export default App
